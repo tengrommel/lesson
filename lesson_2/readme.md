bc36d27fe00bc9dce3c1dd3db45021a1b7b7389a
teng <wwwsousoucom@126.com>
2020-11-14 16:52:25 +0800

    copy clone

--- lesson_2/readme.md
+++ lesson_2/readme.md
@@ -3,7 +3,8 @@
 - 它是一个列表，其中在一端添加项，并从同一端删除项
 - 它主要用于快速变化的对象，解析器和图形处理（如JavaScript Canvas操作）中
 
-# 函数调用需要的东西
+#  Stack与函数
+函数调用需要的东西
 - 功能完成后该去哪里?
 - 指针
 - 参数的存储空间
@@ -11,3 +12,5 @@
 
 # Mutability, Variables, Copying, and Cloning
 
+- Cloning 显示调用
+- Copying 隐式调用
--- lesson_2/v2_mutability/src/main.rs
+++ lesson_2/v2_mutability/src/main.rs
@@ -1,7 +1,19 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Person {
     name: String,
     age: i32
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    pub fn new(x:i32, y: i32) -> Self {
+        Point { x, y }
+    }
 }
 
 fn main() {
@@ -9,10 +21,14 @@ fn main() {
     let y = x;
     x += 5;
     println!("y = {}, x = {}", y, x);
-    let p = Person {
+    let mut p = Person {
         name: "Mat".to_string(),
         age: 35,
     };
-    let p2 = p;
+    let p2 = p.clone();
+    p.name.push_str("teng");
     println!("p = {:?}, p2 = {:?}", p, p2);
+    let pnt = Point::new(3, 4);
+    let pnt2 = pnt;
+    println!("pnt = {:?}, pnt2 = {:?}", pnt, pnt2);
 }
