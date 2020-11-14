d7cf4ca5425c03123eab0c1e754f14863e7f583f
teng <wwwsousoucom@126.com>
2020-11-14 20:58:07 +0800

    pointers

--- lesson_3/v1_pointer/src/main.rs
+++ lesson_3/v1_pointer/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct Person {
     name: String,
     age: i32,
@@ -10,13 +11,31 @@ impl Person {
     pub fn greet(&self) -> String {
         format!("Hi my name is {} ", self.name)
     }
+    pub fn age_up(&mut self, n: i32) {
+        self.age += n
+    }
+    pub fn drop_self(self){}
 }
 
 fn main() {
-    let p = Person::new("matt".to_string(), 35);
+    let mut p = Person::new("matt".to_string(), 35);
     let s = p.greet();
     println!("{}", s);
+
+    let a = get_age(&p);
+    println!("person's age is {}", a);
+
+    // p.drop_self();
+    println!("person's age is {}", a);
+    p.age_up(2);
+    // println!("person's age is {}", a);
+
     let s2 = p.greet();
     println!("really: {}", s2);
+    p.age_up(3);
+    println!("{:?}", p)
+}
 
+fn get_age(s: &Person) -> &i32 {
+    &s.age
 }
